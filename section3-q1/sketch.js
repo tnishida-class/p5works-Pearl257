d1d59bc72cac4d9c92129d45078419309f18dd44
Pearl257 <65203703+Pearl257@users.noreply.github.com>
2020-11-13 15:40:54 +0900

    section3 change

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -5,6 +5,7 @@ function setup(){
   createCanvas(500, 500);
   background(255);
   balloon("One in a million ONCE for TWICE!!!");
+  star(211,32,10)
 }
 
 function balloon(t){
@@ -15,4 +16,15 @@ function balloon(t){
   rect(0, 5, w + p * 2, h + p * 2);
   fill(0);
   text(t, p, h + p);
+}
+function star(cx, cy, r){
+  beginShape();    // 点つなぎを始める
+  for(let i = 0; i < 3; i++){
+    fill(255,105,180);
+    const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    const x = cx + cos(theta) * r;
+    const y = cy + sin(theta) * r;
+    vertex(x, y);  // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE); // 点つなぎを終わる
 }
