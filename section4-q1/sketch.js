f36090301eb48ec9cc9d317812a21d66d0b6674a
Pearl257 <65203703+Pearl257@users.noreply.github.com>
2020-11-21 09:38:35 +0900

    heart beat section

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,11 +7,15 @@ function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
-  size  = 50
+  size = 10
 }
-
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
+
   ellipse(width / 2, height / 2, size);
+  size
+  if (count < 50 ) {  size += 2 }
+  else{
+    size -= 2 }
 }
