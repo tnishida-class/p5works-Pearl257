14c248b3b033c1d70ccb5071d7a000f236288c74
Pearl257 <65203703+Pearl257@users.noreply.github.com>
2020-11-22 20:22:03 +0900

    star update

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -7,30 +7,14 @@ function setup(){
   y = height / 2;
   vx = 8;
   vy = 8;
-  star(250, 50, 40);
+  star(250, 160, 40, 0)
 }
-
 
 function draw(){
   background(160, 192, 255);
-    star(cx, cy, r);
-    beginShape();
-    for(var i = 0; i < 5; i++){
-      let theta = TWO_PI * i * 2 / 5 - HALF_PI;
-      let x = cx + cos(theta) * r;
-      let y = cy + sin(theta) * r;
-      vertex(x,y);
-      fill(0);
-    }
-      endShape(CLOSE);
-}
-
-
-  function draw(){
-  background(160, 192, 255);
-  star(cx, cy, r);
-  cx += 2;
-  cy -= 2;
+  star(x, y, 40, 0);
+  x += 2;
+  y -= 2;
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
   if(y > height){ y = 0; }
@@ -40,16 +24,15 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
-    var theta += angle 
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
   }
+
   endShape(CLOSE);
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
-  star(width*2,height*2)
 }
